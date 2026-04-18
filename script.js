d6c596d83b8921eabaa3d5b9d61b90eba180009b
RobertEClarkson <98361985+RobertEClarkson@users.noreply.github.com>
2026-04-17 19:55:46 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -2,15 +2,26 @@ const gallery = document.getElementById("gallery");
 const threshold = document.getElementById("threshold");
 
 function loadImages(count) {
+  const batch = [];
+
   for (let i = 0; i < count; i++) {
     const img = document.createElement("img");
-
-    //API: Lorem Picsum from the Big List of public API's
-    img.src = `https://picsum.photos/300/200?random=${Date.now() + i}`;     //Without Date.now every image was same
-    img.loading = "lazy";
+    img.src = `https://picsum.photos/300/200?random=${Date.now() + i}`;     //Date.now makes every image different
+    img.style.opacity = 0;                                                  //images start invisible
+    img.style.transform = "translateY(20px)";                               //images also start slightly off grid
 
     gallery.appendChild(img);
+    batch.push(img);
   }
+
+  anime({                                                                   //Makes images fade in one by one
+    targets: batch,                                                         //and return to grid 
+    opacity: 1,
+    translateY: 0,
+    delay: anime.stagger(60),
+    duration: 500,
+    easing: "easeOutQuad"
+  });
 }
 
 const observer = new IntersectionObserver((entries) => {            //Intersection Observer API: checks for the threshold div
